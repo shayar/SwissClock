5179dd6b44bb0e776eb8f873addb01002fd081ec
shayar-monotype <shayar.shrestha@monotype.com>
2020-10-24 13:21:16 +0545

    Hostname

--- main.js
+++ main.js
@@ -111,7 +111,7 @@ function comparer(otherArray) {
 //to add url
 function addUrl(ids) {
 
-    var newUrl = window.location.host;
+    var newUrl = window.location.hostname;
 
     var urlParams = new URLSearchParams(location.search);
     var zoneUrlId = urlParams.getAll('zones');
@@ -131,7 +131,7 @@ function addUrl(ids) {
 //to delete url
 function deleteUrl(id, timezone) {
 
-    var newUrl = window.location.host;
+    var newUrl = window.location.hostname;
 
     var urlParams = new URLSearchParams(location.search);
     var zoneUrlId = urlParams.getAll('zones');
